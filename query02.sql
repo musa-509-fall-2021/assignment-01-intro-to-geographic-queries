d148d990c832dcbb840e7c99a30b6fbd7398eb6a
Alex Nelms <38572146+nelmsal@users.noreply.github.com>
2021-09-08 20:49:32 -0400

    Update query02.sql

--- query02.sql
+++ query02.sql
@@ -7,9 +7,21 @@
 */
 
 -- Enter your SQL query here
-select ...
 
-
+WITH 
+	trips_2020Q2 as (
+    	 SELECT * FROM public.indego_trips_2021_q2
+    ),
+    trips_2019Q2 as (
+    	 SELECT * FROM public.indego_trips_2019_q2
+    ),
+    count_2020Q2 AS (SELECT COUNT(*) FROM trips_2020Q2),
+    count_2019Q2 AS (SELECT COUNT(*) FROM trips_2019Q2)
+SELECT to_char(
+    (count_2020Q2.count - count_2019Q2.count)/count_2020Q2.count::float8, '99999.00%'
+  )
+  AS perc_change
+FROM count_2020Q2, count_2019Q2
 
 /*
   Bonus: If you want to get fancier here, you can cast the result to a string
