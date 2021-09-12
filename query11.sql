89e17b590f968d29f026ae39791972c26845f412
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-12 13:38:27 -0400

    Update query11.sql

--- query11.sql
+++ query11.sql
@@ -4,7 +4,7 @@
 
 -- Enter your SQL query here
 WITH all_distance AS(
-  SELECT indego_station_statuses.id,indego_station_statuses.name,st_distance(the_geom, 'POINT(-75.19268489458014 39.95241719826734)'::geography) AS distance
+  SELECT indego_station_statuses.id,indego_station_statuses.name,st_distance(ST_Transform(the_geom,4326)::geography, ST_Transform('SRID=4326;POINT(-75.19268489458014 39.95241719826734)'::geometry,4326)::geography) AS distance
 FROM indego_station_statuses)
 
 SELECT (AVG(distance))::text||'m'
