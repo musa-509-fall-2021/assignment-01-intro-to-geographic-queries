96bd9fdb25c4b374a76046eafda025657965fbfa
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-12 13:36:27 -0400

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -4,5 +4,5 @@
 */
 
 -- Enter your SQL query here
-SELECT indego_station_statuses.id,indego_station_statuses.name,(st_distance(the_geom, 'POINT(-75.19268489458014 39.95241719826734)'::geography))::text||'m' AS distance
+SELECT indego_station_statuses.id,indego_station_statuses.name,st_distance(ST_Transform(the_geom,4326)::geography, ST_Transform('SRID=4326;POINT(-75.19268489458014 39.95241719826734)'::geometry,4326)::geography) AS distance
 FROM indego_station_statuses 
