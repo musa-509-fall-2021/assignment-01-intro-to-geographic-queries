46a8846a9589d849341fb540ff1e2740f9de7e9c
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-11 23:20:37 -0400

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -3,4 +3,11 @@
 */
 
 -- Enter your SQL query here
-select ...
+WITH distance_all AS (SELECT indego_station_statuses.id,indego_station_statuses.name,st_distance(the_geom, 'POINT(-75.19268489458014 39.95241719826734)'::geography) AS distance
+FROM indego_station_statuses)
+
+SELECT COUNT(*) 
+FROM distance_all
+WHERE distance < 1000
+
+--result:14
