ece9e3ce5b11b817d1ccd50acbda7f62893602b5
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-12 11:50:49 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -6,4 +6,12 @@
 SELECT round(avg(duration),2) AS duration_mean
 FROM indego_trips_2020_q2
 
---result:39.24
+--result:39.24 
+
+/*
+What do you notice about the difference in trip lengths? Give a few explanations for why there could be a difference here.
+
+Answer: The average duration of a trip for 2020 is longer than that for 2019. 
+
+
+*/
