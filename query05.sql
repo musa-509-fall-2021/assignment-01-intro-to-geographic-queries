21e6c3a3efbf9aff9af84ca94aaf29aa9f1b6e62
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-12 12:02:02 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -11,3 +11,10 @@ WHERE indego_trips_2020_q2.duration IN
  
  
 --result:1440
+
+/*
+Why are there so many trips of this duration?
+
+Answer: According to Indego official website, the maximum rental time permitted is 24 hours. So those bikes that did not been returned within 24 hours were automatically returned.
+
+*/
