065288eef4055b7a73e0ccad7a8d77c4258d9177
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-12 12:10:42 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -12,6 +12,7 @@ FROM indego_trips_2020_q2
 What do you notice about the difference in trip lengths? Give a few explanations for why there could be a difference here.
 
 Answer: The average duration of a trip for 2020 is longer than that for 2019. 
+Comparing to 2019, in 2020 Indego expanded their stations to more coummunities where couldn't be touched by Indego bicycles in the past. And because of increasing amount of electric bicycles, people could reach further places. Consequently, the lengths of distance increased.
 
 
 */
