d3dbe2ebdfd67ee3929a2d11a57f3387c819c89f
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-12 13:42:59 -0400

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -6,9 +6,12 @@
 */
 
 -- Enter your SQL query here
-SELECT id,name,st_distance(the_geom, 'POINT(-75.19268489458014 39.95241719826734)'::geography)
+SELECT id,name, st_distance(ST_Transform(the_geom,4326)::geography, 
+                                  ST_Transform('SRID=4326;POINT(-75.19268489458014 39.95241719826734)'::geometry,4326)::geography)
 FROM indego_station_statuses
-WHERE st_distance(the_geom, 'POINT(-75.19268489458014 39.95241719826734)'::geography) IN 
-(SELECT MAX(st_distance(the_geom, 'POINT(-75.19268489458014 39.95241719826734)'::geography))
+WHERE  st_distance(ST_Transform(the_geom,4326)::geography, 
+                                  ST_Transform('SRID=4326;POINT(-75.19268489458014 39.95241719826734)'::geometry,4326)::geography) IN 
+(SELECT MAX( st_distance(ST_Transform(the_geom,4326)::geography, 
+                                  ST_Transform('SRID=4326;POINT(-75.19268489458014 39.95241719826734)'::geometry,4326)::geography))
  FROM indego_station_statuses)
  
