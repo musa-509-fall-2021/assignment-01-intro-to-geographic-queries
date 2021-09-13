0170844acfe5d446c3d9c1a15d35289eeae14183
Alex Nelms <38572146+nelmsal@users.noreply.github.com>
2021-09-13 17:17:48 -0400

    Update query03.sql

--- query03.sql
+++ query03.sql
@@ -3,4 +3,11 @@
 */
 
 -- Enter your SQL query here
-select ...
+
+WITH 
+    full_time as (
+        SELECT EXTRACT(EPOCH FROM (TO_TIMESTAMP(end_time, 'MM/DD/YYYY HH24:MI') - TO_TIMESTAMP(start_time, 'MM/DD/YYYY HH24:MI'))) AS time_difference
+      	FROM public.indego_trips_2020_q2
+    )
+SELECT *
+FROM full_time
