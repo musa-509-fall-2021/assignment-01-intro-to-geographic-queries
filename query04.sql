c2fc00a9f05e36eb8bc362f86f3f63c2808c75ae
Alex Nelms <38572146+nelmsal@users.noreply.github.com>
2021-09-14 18:17:15 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -3,4 +3,16 @@
 */
 
 -- Enter your SQL query here
-select ...
+
+WITH 
+    full_time as (
+        SELECT 
+      CAST(EXTRACT(EPOCH FROM (TO_TIMESTAMP(end_time, 'MM/DD/YYYY HH24:MI') - TO_TIMESTAMP(start_time, 'MM/DD/YYYY HH24:MI'))) AS int) AS time_difference
+      	FROM public.indego_trips_2020_q2
+    )
+SELECT 
+	AVG(time_difference)/60 as avg_minutes_per_trip
+FROM full_time
+WHERE time_difference IS NOT NULL
+
+-- Average Duration of 49.781 Minutes per trip in 2020 Q2
