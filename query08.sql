9c6f8a98227c3dbd321994f342e2984662f7746c
maxmasudafarkas <68787255+maxmasudafarkas@users.noreply.github.com>
2021-09-15 13:22:47 -0400

    Update query08.sql

--- query08.sql
+++ query08.sql
@@ -3,4 +3,11 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT 
+	start_station,
+  COUNT(*) AS number_of_trips
+FROM public.indego_trips_2019_q2
+WHERE EXTRACT(HOUR FROM start_time) > 7 and EXTRACT(HOUR FROM start_time) < 10
+GROUP BY start_station
+ORDER BY number_of_trips DESC
+LIMIT 5
