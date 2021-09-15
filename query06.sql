2424aa9c95d493e660408200aadcf2346cb5f58f
johnatawncleemntawn <johnatawnclementawn@gmail.com>
2021-09-15 16:10:38 -0400

    Query 6 Answer

--- query06.sql
+++ query06.sql
@@ -1,6 +1,18 @@
+-- Johnathan Clementi --
+-- MUSA 509 Geospatial Cloud Computing & Vizualization --
+-- HW #1 2021-09-19 --
+-- Query #6 --
+
 /*
   How many trips were shorter than 10 minutes?
 */
 
 -- Enter your SQL query here
-select ...
+with indego_trips_Q2_19and20 as (
+  select duration from indego_trips_2019_q2
+  union
+  select duration from indego_trips_2020_q2
+)
+
+select count(duration > 10) as tripsLess10Min
+from indego_trips_Q2_19and20
