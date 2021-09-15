5ec1e2f03eb6b09ada0b447fe11f865c2316cc57
johnatawncleemntawn <johnatawnclementawn@gmail.com>
2021-09-15 16:09:49 -0400

    Query 4 Answer

--- query04.sql
+++ query04.sql
@@ -1,6 +1,12 @@
+-- Johnathan Clementi --
+-- MUSA 509 Geospatial Cloud Computing & Vizualization --
+-- HW #1 2021-09-19 --
+-- Query #4 --
+
 /*
   What is the average duration of a trip for 2020?
 */
 
 -- Enter your SQL query here
-select ...
+select round(avg(duration),2) as avgDurQ2_2020
+from indego_trips_2020_q2
