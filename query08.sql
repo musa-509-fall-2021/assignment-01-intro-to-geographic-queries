eb0c39ecf52d8c3dd37c247836ca3111d2f96218
Jeff Stern <jeffstern.ny@gmail.com>
2021-09-14 21:35:20 -0400

    q 8 & 9

--- query08.sql
+++ query08.sql
@@ -3,4 +3,9 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT s.station_name, COUNT(t.start_station) as frequency FROM indego_trips_2019_q2 AS t
+INNER JOIN indego_stations AS s ON t.start_station = s.station_id
+WHERE DATE_PART('h',t.start_time) >= 7 and DATE_PART('h',t.start_time) < 10
+GROUP BY s.station_name
+ORDER BY frequency desc
+LIMIT 5
--- query09.sql
+++ query09.sql
@@ -6,4 +6,11 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT start_year, passholder_type, ride_count FROM
+  (SELECT passholder_type, COUNT(passholder_type) AS ride_count, DATE_PART('y',start_time::date) AS start_year
+  FROM indego_trips_2019_q2
+  GROUP BY passholder_type, start_year
+  UNION ALL
+  SELECT passholder_type, COUNT(passholder_type) AS ride_count, DATE_PART('y',start_time::date) AS start_year
+  FROM indego_trips_2020_q2
+  GROUP BY passholder_type, start_year) AS u
