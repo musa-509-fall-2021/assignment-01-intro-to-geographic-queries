39b543ea5c1f33a01fd4901e008bc9167173fac7
johnatawncleemntawn <johnatawnclementawn@gmail.com>
2021-09-15 16:07:41 -0400

    Query1 Answer

--- query01.sql
+++ query01.sql
@@ -1,3 +1,8 @@
+-- Johnathan Clementi --
+-- MUSA 509 Geospatial Cloud Computing & Vizualization --
+-- HW #1 2021-09-19 --
+-- Query #1 --
+
 /*
   Example: How many bike trips in Q2 2019
 */
@@ -5,3 +10,7 @@
 -- Enter your SQL query here
 select count(*)
 from indego_trips_2019_q2
+
+/* 
+The answer is 206354 bike trips in Q2 of 2019
+ */
