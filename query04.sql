8012a7e5cbc18625958415b42392eb53ecf9cf52
austinlwheat <89946784+austinlwheat@users.noreply.github.com>
2021-09-15 22:33:12 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -1,6 +1,8 @@
 /*
   What is the average duration of a trip for 2020?
-  Answer - 39.23. One possible reason I can think of as to the increase would be Indego increasing the max amount of time they allow customers to use a device from one year to the next.
+  Answer - 39.23. 
+  One possible reason I can think of as to the increase would be Indego increasing the max amount of time they allow customers to rent a bike from one year to the next.
+  With the pandemic they also may have opted to allow increased rental times to make up for lost revenue due to the decreased individual trip quantities.
 */
 
 -- Enter your SQL query here
