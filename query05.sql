59b9673878f49f43764151b08c37d121a3ada29e
hlechuan <89944391+hlechuan@users.noreply.github.com>
2021-09-16 17:29:13 -0400

    Update query05.sql
    initial response

--- query05.sql
+++ query05.sql
@@ -3,4 +3,16 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT MAX(DURATION)
+ FROM indego_trips_2019_q2
+ 
+-- and 
+SELECT MAX(DURATION)
+ FROM indego_trips_2020_q2
+
+--both returned with 1440 mins = 24 hours
+ /*
+Why are there so many trips of this duration?
+
+Answer: maybe those users didn't even return their bicycle and the max duration the system generate is 1440 (mins)
+*/
