aa363737def6c00d1127b18c51969196eb9c9ab6
austinlwheat <89946784+austinlwheat@users.noreply.github.com>
2021-09-15 22:30:49 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -1,6 +1,11 @@
 /*
   What is the longest duration trip?
+  Answer - 1440. 1440 minutes = 24 hours. 
+  I would venture to guess that the max amount of time for which you can rent a bike is 24 hours. 
+  After that, the rental likely maxes out and once again becomes available for rent.
 */
 
 -- Enter your SQL query here
-select ...
+select *
+from indego_trips_2020_q2
+order by duration desc
