0e073af9d2aea56bd1b9c6eea4bd3c65037df0df
maxmasudafarkas <68787255+maxmasudafarkas@users.noreply.github.com>
2021-09-17 16:02:13 -0400

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -6,4 +6,7 @@
 */
 
 -- Enter your SQL query here
-select ...
+select *
+from public.a_tgbqkercddrih5lwa1zg
+order by the_geom <-> st_setsrid(st_makepoint(-75.19266128540039, 39.95223725519681), 4326) ASC
+limit 1
