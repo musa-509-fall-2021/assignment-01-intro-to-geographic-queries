704e2986b3db40543cd9524869dcd8a2b8304390
Sisun Cheng <997723678@qq.com>
2021-09-16 23:08:49 -0400

    Update query05.sql
    Updated with solution.

--- query05.sql
+++ query05.sql
@@ -3,4 +3,14 @@
 */
 
 -- Enter your SQL query here
-select ...
+select max(duration) as max_2020
+from indego_trips_2020_q2
+
+/*
+Why are there so many trips of this duration?
+
+Answer:
+
+Because 1440 minutes equal to 24 hours which is the maximum duration of a single day.
+
+*/
