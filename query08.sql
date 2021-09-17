990a8eb18a82667c09878826773ddd093e310933
Sisun Cheng <997723678@qq.com>
2021-09-16 23:08:58 -0400

    Update query08.sql
    Updated with solution.

--- query08.sql
+++ query08.sql
@@ -3,4 +3,14 @@
 */
 
 -- Enter your SQL query here
-select ...
+select start_station as station, count(start_station) as num
+from indego_trips_2019_q2
+where extract(hour from end_time)<10 and extract(hour from start_time)>=7
+group by start_station
+union
+select end_station as station, count(end_station) as num
+from indego_trips_2019_q2
+where extract(hour from end_time)<10 and extract(hour from start_time)>=7
+group by end_station
+order by num desc
+limit 5
