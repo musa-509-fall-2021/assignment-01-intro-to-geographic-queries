35eed34690a6691f2d30a4ccd95456c241cf015c
Sisun Cheng <997723678@qq.com>
2021-09-16 23:40:29 -0400

    Update query12.sql
    Updated with solution.

--- query12.sql
+++ query12.sql
@@ -3,4 +3,10 @@
 */
 
 -- Enter your SQL query here
-select ...
+with distances as(
+select cartodb_id, the_geom, id, name, addressstreet, 
+st_distance(ST_Transform(the_geom::geometry, 3857), ST_Transform('SRID=4326;POINT(-75.19266128540039 39.952204357143664)'::geometry, 3857)) as distance_meter
+from station_status)
+select count(*)
+from distances
+where distance_meter <1000
