c8e112465a145d8e4a71e537c7273351196462d4
Sisun Cheng <997723678@qq.com>
2021-09-16 23:40:23 -0400

    Update query10.sql
    Updated with solution.

--- query10.sql
+++ query10.sql
@@ -4,4 +4,6 @@
 */
 
 -- Enter your SQL query here
-select ...
+select cartodb_id, the_geom, id, name, addressstreet, 
+st_distance(ST_Transform(the_geom::geometry, 3857), ST_Transform('SRID=4326;POINT(-75.19266128540039 39.952204357143664)'::geometry, 3857)) as distance_meter
+from station_status
