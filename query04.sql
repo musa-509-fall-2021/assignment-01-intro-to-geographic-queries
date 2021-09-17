4f40ab0cc885b0969492ddeb413902d9b31b2741
Sisun Cheng <997723678@qq.com>
2021-09-16 23:08:46 -0400

    Update query04.sql
    Updated with solution.

--- query04.sql
+++ query04.sql
@@ -3,4 +3,17 @@
 */
 
 -- Enter your SQL query here
-select ...
+select avg(duration) as average_duration
+from indego_trips_2020_q2
+
+/*
+What do you notice about the difference in trip lengths? 
+Give a few explanations for why there could be a difference here.
+
+Answer:
+
+The average duration of trips in 2020 is significantly longer than that in 2019.
+Possible explanations might due to the COVID, more people choose to ride a bike rather than 
+take public transportations, even if it is a long travel.
+
+*/
