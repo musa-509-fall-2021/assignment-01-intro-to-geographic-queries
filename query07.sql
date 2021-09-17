c7cc78bc5d6f6547bb26121eec7c14df5595a999
johnatawncleemntawn <johnatawnclementawn@gmail.com>
2021-09-17 18:08:26 -0400

    Queries 7 and 8 not finished, need to pull changes to main repo

--- query07.sql
+++ query07.sql
@@ -1,6 +1,22 @@
+-- Johnathan Clementi --
+-- MUSA 509 Geospatial Cloud Computing & Vizualization --
+-- HW #1 2021-09-19 --
+-- Query #7 --
+
 /*
   How many trips started on one day and ended in the next?
 */
 
 -- Enter your SQL query here
-select ...
+with indego_trips_Q2_19and20 as (
+  /* select extract(day from start_time), 
+        extract(day from end_time) 
+        from indego_trips_2019_q2
+  union */
+  select /* extract(day from */ to_date(start_time,'MM/DD/YYYY')/* ) */, 
+        /* extract(day from */ to_date(end_time,'MM/DD/YYYY')/* )  */
+        from indego_trips_2020_q2
+)
+
+select * /* count(duration > 10) as tripsLess10Min */
+from indego_trips_Q2_19and20
--- query08.sql
+++ query08.sql
@@ -1,6 +1,17 @@
 /*
-  Give the five most popular stations between 7am and 10am in 2019.
+  Give the five most popular starting stations between 7am and 10am in 2019.
 */
 
 -- Enter your SQL query here
-select ...
+with startStationTrips as (
+  select start_station, count(*)
+        filter (where (extract(hour from start_time) >= 7) and
+                      (extract(hour from end_time) <= 10)
+                )
+  from indego_trips_2019_q2
+  group by start_station
+)
+
+
+select *
+from startStationTrips
