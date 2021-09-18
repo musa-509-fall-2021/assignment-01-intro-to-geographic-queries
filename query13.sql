a873334de338312948480be55f4d3a3c8350dba5
yzy2000 <yzy2000@upenn.edu>
2021-09-18 14:30:14 -0400

    completed query 13

--- query13.sql
+++ query13.sql
@@ -6,4 +6,11 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT indego_station_statuses.id,
+		indego_station_statuses.name,
+      st_transform(st_setsrid(the_geom, 4326), 3857) as the_geom_webmercator,
+      st_distance(st_transform(st_setsrid(the_geom, 4326), 3857), st_transform(st_setsrid(st_makepoint(-75.1926 ,39.9522), 4326), 3857)
+                 ) AS distance
+from indego_station_statuses
+order by distance desc
+limit 1
