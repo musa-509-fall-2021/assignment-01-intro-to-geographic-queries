0d0da3753cbb68f4c4acbc107e655391c4cdbbb1
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 22:24:09 -0400

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -11,7 +11,10 @@ name 3132.612699968969
 
 -- Enter your SQL query here
 with distance as ( 
-  select id, name, st_distance(ST_Transform(the_geom::geometry, 3857),ST_Transform('SRID=4326; POINT( -75.19245084288279 39.952469494960795)'::geometry, 3857)) as dist_m
+  select id, name, 
+  st_distance(ST_Transform(the_geom::geometry, 3857),
+              ST_Transform('SRID=4326; POINT( -75.19245084288279 39.952469494960795)'::geometry, 3857)
+             ) as dist_m
   from indego_station_statues
 )
 
