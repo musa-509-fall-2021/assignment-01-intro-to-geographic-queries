45c1e85865ceb3413f302a5e3a12fde42a5782e3
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 22:23:46 -0400

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -3,7 +3,19 @@
 
   Your query should return only one line, and only gives the station id, station
   name, and distance from Meyerson Hall.
+  
+id  3255
+number Chestnut & Broad
+name 3132.612699968969
 */
 
 -- Enter your SQL query here
-select ...
+with distance as ( 
+  select id, name, st_distance(ST_Transform(the_geom::geometry, 3857),ST_Transform('SRID=4326; POINT( -75.19245084288279 39.952469494960795)'::geometry, 3857)) as dist_m
+  from indego_station_statues
+)
+
+SELECT id, name, dist_m
+FROM distance
+ORDER BY distance DESC
+LIMIT 1
