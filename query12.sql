06fb1a6fe7675549ffe99d0e04bd47370570aa47
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 21:42:23 -0400

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -1,6 +1,14 @@
 /*
   How many stations are within 1km of Meyerson Hall?
+  ANSWER: 8
 */
 
 -- Enter your SQL query here
-select ...
+with distance as ( 
+  select st_distance(ST_Transform(the_geom::geometry, 3857),ST_Transform('SRID=4326; POINT( -75.19245084288279 39.952469494960795)'::geometry, 3857)) as dist_m
+  from indego_station_statues
+)
+
+SELECT COUNT(*) 
+FROM distance
+WHERE dist_m < 1000 
