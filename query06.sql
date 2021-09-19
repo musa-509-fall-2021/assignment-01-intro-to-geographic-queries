f22ba37b4f8b856b57f90193a7097fd470354fed
yyebei <90070226+yyebei@users.noreply.github.com>
2021-09-19 15:14:54 -0400

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -12,14 +12,3 @@ select count(*)as sum1
 from indego_trips_2019_q2
 where(duration)<10
 )as total
-
-
-select sum(sum1) from(
-select count(*)as sum1
-from indego_trips_2020_q2
-where duration <10
-union all
-select count(*)as sum1
-from indego_trips_2019_q2
-where duration <10
-)as total
