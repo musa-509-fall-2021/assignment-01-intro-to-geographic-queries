3c581f647fb55bfe93533f1f45bd66becb1eb40f
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 22:48:36 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -1,6 +1,7 @@
 /*
   What is the average duration of a trip for 2020?
+  ANSWER:39.23600913251798
 */
 
 -- Enter your SQL query here
-SELECT AVG(duration) FROM indego_trips_2020_q2;
+SELECT AVG(duration) FROM indego_trips_2020_q2
