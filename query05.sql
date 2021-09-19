03169e301874658b1f1d0bae0b575d13d7e39b08
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 23:16:32 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -1,6 +1,7 @@
 /*
-  What is the longest duration trip?
-  ANSWER: For 2020 the longest duration trip is 1440, while for 2019 it is 1440.
+  What is the longest duration trip? Why are there so many trips of this duration?
+  ANSWER: For 2020 the longest duration trip is 1440, while for 2019 it is also 1440.
+  I think 
 */
 
 -- Enter your SQL query here
