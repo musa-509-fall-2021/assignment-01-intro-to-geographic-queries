d3a5a6ae1de1214fa93b4e3aab67d7b2b732f077
yyebei <90070226+yyebei@users.noreply.github.com>
2021-09-19 00:01:33 -0400

    querry04

--- query04.sql
+++ query04.sql
@@ -5,3 +5,7 @@
 -- Enter your SQL query here
 SELECT AVG (duration)
 FROM indego_trips_2020_q2
+
+--I think it's because under the condition of covid 19,
+--people prefer to ride bike instead of taking public transportation,which lead to longer duration
+--of each trip.
--- query05.sql
+++ query05.sql
@@ -3,4 +3,8 @@
 */
 
 -- Enter your SQL query here
-select ...
+select max
+from
+(select max(duration)from indego_trips_2020_q2
+ union
+ select max(duration)from indego_trips_2020_q2)d
