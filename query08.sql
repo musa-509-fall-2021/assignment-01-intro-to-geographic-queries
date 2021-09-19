478a0a9b09d0126f158c68b124cbcfb3ef6e2342
Sisun Cheng <997723678@qq.com>
2021-09-18 21:46:58 -0400

    Update query08.sql
    Updated with new solution.

--- query08.sql
+++ query08.sql
@@ -3,6 +3,10 @@
 */
 
 -- Enter your SQL query here
+/*
+ The first version, the five most popular stations between 7 and 10 
+ (including starting and ending stations)
+
 select start_station as station, count(start_station) as num
 from indego_trips_2019_q2
 where extract(hour from end_time)<10 and extract(hour from start_time)>=7
@@ -13,4 +17,14 @@ from indego_trips_2019_q2
 where extract(hour from end_time)<10 and extract(hour from start_time)>=7
 group by end_station
 order by num desc
+limit 5
+*/
+
+/* the five most popular starting stations between 7am and 10am*/
+select start_station, count(*) as numbers
+from indego_trips_2019_q2
+where extract(hour from end_time)<10
+and extract(hour from start_time)>=7
+group by start_station
+order by count(*) desc
 limit 5
