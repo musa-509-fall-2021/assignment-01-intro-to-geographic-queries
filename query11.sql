e6fb4aead5c8a5d60b052987759b5a0b5fe44011
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 20:26:57 -0400

    Update query11.sql

--- query11.sql
+++ query11.sql
@@ -1,6 +1,12 @@
 /*
   What is the average distance (in meters) of all stations from Meyerson Hall?
+  ANSWER:3722.9228142180627
 */
 
 -- Enter your SQL query here
-select ...
+select AVG(
+  st_distance(ST_Transform(the_geom::geometry, 3857),
+              ST_Transform('SRID=4326; 
+                           POINT( -75.19245084288279 39.952469494960795)'::geometry, 
+                           3857))
+              ) as average_distance from indego_station_statues;
