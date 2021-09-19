91ca4b6783727ee810ebf5d4a56fbba44f5a39ed
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-18 22:52:07 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -1,7 +1,8 @@
 /*
   What is the longest duration trip?
+  ANSWER: For 2020 the longest duration trip is 1440, while for 2019 it is 1440.
 */
 
 -- Enter your SQL query here
-SELECT MAX(duration) FROM indego_trips_2020_q2;
-SELECT MAX(duration) FROM indego_trips_2019_q2;
+SELECT MAX(duration) FROM indego_trips_2020_q2
+SELECT MAX(duration) FROM indego_trips_2019_q2
