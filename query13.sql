a7b8196a03158d72e31c68446ca2426476d8e792
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-20 09:42:20 -0400

    Update query13.sql

--- query13.sql
+++ query13.sql
@@ -7,10 +7,11 @@
 
 -- Enter your SQL query here
 SELECT id,
-     name, 
-     st_transform(st_setsrid(the_geom, 4326), 3857) as the_geom_webmercator,
+     name,
      st_distance(st_transform(st_setsrid(the_geom,4326),3857),st_transform(st_setsrid(st_makepoint(-75.19273854113067,39.95235140309713),4326),3857)) as distance
 FROM indego_station_statuses
 WHERE  st_distance(st_transform(st_setsrid(the_geom,4326),3857),st_transform(st_setsrid(st_makepoint(-75.19273854113067,39.95235140309713),4326),3857)) IN 
 (SELECT MAX(st_distance(st_transform(st_setsrid(the_geom,4326),3857),st_transform(st_setsrid(st_makepoint(-75.19273854113067,39.95235140309713),4326),3857)))
  FROM indego_station_statuses)
+
+-- answer: 15th&Kitty Hawk
