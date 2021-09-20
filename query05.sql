e8ef6dbc167fd2a3c08988fcfbfda1aea5a9f763
maxmasudafarkas <68787255+maxmasudafarkas@users.noreply.github.com>
2021-09-20 09:18:29 -0400

    Update query05.sql

--- query05.sql
+++ query05.sql
@@ -2,6 +2,12 @@
   What is the longest duration trip?
 */
 
--- Enter your SQL query here
-SELECT MAX(duration)
-FROM public.indego_trips_2020_q2
+-- For 2019:
+SELECT COUNT(trip_id)
+FROM indego_trips_2019_q2
+WHERE duration = 1440
+
+-- For 2020:
+SELECT COUNT(trip_id)
+FROM indego_trips_2020_q2
+WHERE duration = 1440
