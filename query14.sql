2c3959933e0f9943d41fcf9f2b203b4c7b64b9b2
austinlwheat <89946784+austinlwheat@users.noreply.github.com>
2021-09-20 13:09:51 -0400

    Update query14.sql

--- query14.sql
+++ query14.sql
@@ -5,5 +5,10 @@
   name, and distance from Meyerson Hall.
 */
 
+Answer - 201.08558757
+
 -- Enter your SQL query here
-select ...
+SELECT name, st_distance(the_geom, st_setsrid(st_makepoint(-75.19266128540039, 39.952233142941054), 4326), FALSE) as distance_from_meyerson
+FROM station_status
+ORDER BY distance_from_meyerson asc
+LIMIT 1
