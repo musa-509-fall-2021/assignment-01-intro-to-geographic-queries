9668353fedc64b62b8b865c43bfdc12d3c21c453
Sisun Cheng <997723678@qq.com>
2021-09-20 15:40:13 -0400

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -3,6 +3,18 @@
 */
 
 -- Enter your SQL query here
-select count(*)
+
+with sum_trips as(
+select cartodb_id, duration 
 from indego_trips_2020_q2
+union 
+select cartodb_id, duration 
+from indego_trips_2019_q2)
+
+select count(*) from sum_trips
 where  duration < 10
+
+
+/*
+The query result: 119596
+*/
