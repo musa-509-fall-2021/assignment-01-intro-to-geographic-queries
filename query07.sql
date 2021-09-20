492e0d9fb625e67ef0e6cde44abd577e7daa61bb
Sisun Cheng <997723678@qq.com>
2021-09-20 16:42:26 -0400

    Update query07.sql

--- query07.sql
+++ query07.sql
@@ -3,16 +3,21 @@
 */
 
 -- Enter your SQL query here
-with results as (
-  select to_timestamp(start_time, 'mm/dd/yyyy hh24:mi') as stime, to_timestamp(end_time, 'mm/dd/yyyy hh24:mi') as etime
-  from indego_trips_2020_q2
-  where (extract(month from to_timestamp(end_time, 'mm/dd/yyyy hh24:mi')) - extract(month from to_timestamp(start_time, 'mm/dd/yyyy hh24:mi'))=0
-  and extract(day from to_timestamp(end_time, 'mm/dd/yyyy hh24:mi')) - extract(day from to_timestamp(start_time, 'mm/dd/yyyy hh24:mi'))=1)
-  or
-  (extract(month from to_timestamp(end_time, 'mm/dd/yyyy hh24:mi'))=5 and extract(month from to_timestamp(start_time, 'mm/dd/yyyy hh24:mi'))=4
-  and extract(day from to_timestamp(end_time, 'mm/dd/yyyy hh24:mi'))=1 and extract(day from to_timestamp(start_time, 'mm/dd/yyyy hh24:mi'))=30)
-  or
-  (extract(month from to_timestamp(end_time, 'mm/dd/yyyy hh24:mi'))=6 and extract(month from to_timestamp(start_time, 'mm/dd/yyyy hh24:mi'))=5
-  and extract(day from to_timestamp(end_time, 'mm/dd/yyyy hh24:mi'))=1 and extract(day from to_timestamp(start_time, 'mm/dd/yyyy hh24:mi'))=31)
-  )
-select count(*) from results
+  with all_records as(
+  with all_time as(
+  select  trip_id, start_time, end_time 
+  from indego_trips_2019_q2
+  union
+  select trip_id, to_timestamp(start_time, 'mm/dd/yyyy hh24:mi') as start_time, to_timestamp(end_time, 'mm/dd/yyyy hh24:mi') as end_time
+  from indego_trips_2020_q2)
+  select trip_id, extract(month from start_time) as sm, extract(day from start_time) as sd,
+  extract(month from end_time) as em, extract(day from end_time) as ed  from all_time)
+  select count(*) from all_records
+  where  (em-sm=0 and ed-sd=1)
+  or (em=5 and sm=4 and ed=1 and sd=30)
+  or (em=6 and sm=5 and ed=1 and sd=31)
+
+
+/*
+The query result: 4236
+*/
