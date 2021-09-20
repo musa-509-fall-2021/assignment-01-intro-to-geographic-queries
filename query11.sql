cdbb5a69744de98ab03d70d57e573801a4b2a816
austinlwheat <89946784+austinlwheat@users.noreply.github.com>
2021-09-20 12:55:03 -0400

    Update query11.sql

--- query11.sql
+++ query11.sql
@@ -3,4 +3,6 @@
 */
 
 -- Enter your SQL query here
-select ...
+
+SELECT AVG(st_distance(the_geom, st_setsrid(st_makepoint(-75.19266128540039, 39.952233142941054), 4326), FALSE)) as distance_from_meyerson
+FROM station_status
