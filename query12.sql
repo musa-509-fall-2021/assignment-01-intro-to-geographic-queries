6e4ad85762864ef16f0d7f27b41da3b3f2f0e9eb
maxmasudafarkas <68787255+maxmasudafarkas@users.noreply.github.com>
2021-09-20 08:26:04 -0400

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -3,4 +3,13 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT 
+	ST_Distance(
+      ST_Transform(the_geom, 3857), 
+      ST_Transform('SRID=4326;POINT(-75.19266128540039 39.95223725519681)'::geometry, 3857))
+      AS distance_to_meyerson,
+    *
+from public.a_tgbqkercddrih5lwa1zg
+WHERE ST_Distance(
+      ST_Transform(the_geom, 3857), 
+      ST_Transform('SRID=4326;POINT(-75.19266128540039 39.95223725519681)'::geometry, 3857)) <= 1000
