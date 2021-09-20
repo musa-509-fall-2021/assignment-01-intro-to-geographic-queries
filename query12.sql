0622b5cfc6b86f02a8c3fe4bdf5380d3d167454d
austinlwheat <89946784+austinlwheat@users.noreply.github.com>
2021-09-20 12:51:48 -0400

    Update query12.sql

--- query12.sql
+++ query12.sql
@@ -2,5 +2,9 @@
   How many stations are within 1km of Meyerson Hall?
 */
 
+ANSWER - 14
+
 -- Enter your SQL query here
-select ...
+
+SELECT COUNT(*) as close_to_meyerson FROM station_status
+WHERE st_distance(the_geom, st_setsrid(st_makepoint(-75.19266128540039, 39.952233142941054), 4326), FALSE) < 1000
