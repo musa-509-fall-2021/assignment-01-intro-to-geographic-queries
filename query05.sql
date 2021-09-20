3a38570164c91025385f11d2278429faa75b8313
benaiken <90015778+benaiken@users.noreply.github.com>
2021-09-20 14:45:00 -0400

    Completed

--- query05.sql
+++ query05.sql
@@ -3,13 +3,12 @@
 */
 
 -- Enter your SQL query here
-select max(duration) 
-from indego_trips_2019_q2
-
-RESULT: 1440
-
-
+with q2_durations as (
+SELECT duration FROM public.indego_trips_2019_q2
+UNION ALL 
+SELECT duration FROM public.indego_trips_2020_q2
+)
 select max(duration)
-from indego_trips_2020_q2
+from q2_durations
 
 RESULT: 1440
