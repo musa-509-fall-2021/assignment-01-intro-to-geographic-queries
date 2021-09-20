8cb2dd12f205ec5b51149e1400e6bb0981684cb0
maxmasudafarkas <68787255+maxmasudafarkas@users.noreply.github.com>
2021-09-20 09:04:46 -0400

    Update query07.sql

--- query07.sql
+++ query07.sql
@@ -3,4 +3,7 @@
 */
 
 -- Enter your SQL query here
-select ...
+SELECT 
+	COUNT(trip_id)
+FROM indego_trips_2020_q2 
+WHERE EXTRACT(DAY from to_timestamp(start_time, 'MM-DD-YYYY HH24:MI')) + 1 = EXTRACT(DAY from to_timestamp(end_time, 'MM-DD-YYYY HH24:MI'))
