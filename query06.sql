a68800e034d527d8fa7fe9e864cb55082ba906c9
maxmasudafarkas <68787255+maxmasudafarkas@users.noreply.github.com>
2021-09-20 17:23:09 -0400

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -2,7 +2,12 @@
   How many trips were shorter than 10 minutes?
 */
 
--- Enter your SQL query here
+-- For 2020:
 SELECT COUNT(*)
 FROM public.indego_trips_2020_q2
+WHERE duration < 10
+
+-- For 2019:
+SELECT COUNT(*)
+FROM public.indego_trips_2019_q2
 WHERE duration < 10
