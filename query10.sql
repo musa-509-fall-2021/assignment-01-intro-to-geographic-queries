2b32d36df7d8e9d694d1fe5ee6089a9e8a74db70
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-20 22:36:56 -0400

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -5,6 +5,6 @@
 */
 
 -- Enter your SQL query here
-select st_distance(ST_Transform(the_geom::geometry, 3857),
+select name, st_distance(ST_Transform(the_geom::geometry, 3857),
                    ST_Transform('SRID=4326; POINT( -75.19245084288279 39.952469494960795)'::geometry, 3857))
                    as distance from indego_station_statues
