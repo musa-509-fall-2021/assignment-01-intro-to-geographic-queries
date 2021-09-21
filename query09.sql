848ba1fd7d5ea388d8f9991710419d1a73dc7a69
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-20 22:14:55 -0400

    Update query09.sql
    2019：
    Day Pass 34197
    Indego30 133344
    Indego365 37843
    IndegoFlex 851
    Walk-up 84
    NULL 35
    
    2020:
    Day Pass  38165
    Indego30 129905
    Indego365 18515
    Indego Flex 1

--- query09.sql
+++ query09.sql
@@ -6,8 +6,20 @@
 */
 
 -- Enter your SQL query here
-SELECT passholder_type,count(*)
-FROM indego_trips_2019_q2
-GROUP BY passholder_type
 
+
+with first_table as (
+  SELECT a.passholder_type as type_1,count(*) as number_2019 
+  FROM indego_trips_2019_q2 AS a
+  GROUP BY 1)
+  ,second_table as (
+  SELECT b.passholder_type as type_2,count(*) AS number_2020
+  FROM indego_trips_2020_q2 AS b
+  GROUP BY 1)
+ 
+select *
+from first_table
+left join second_table
+	on second_table.type_2
+    = first_table.type_1
 
