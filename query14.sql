a9f91d43927c6dd58515b34a8b460c6447494a80
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-20 22:16:31 -0400

    Update query14.sql
    34th&Spruce

--- query14.sql
+++ query14.sql
@@ -14,4 +14,4 @@ WHERE  st_distance(st_transform(st_setsrid(the_geom,4326),3857),st_transform(st_
 (SELECT MIN(st_distance(st_transform(st_setsrid(the_geom,4326),3857),st_transform(st_setsrid(st_makepoint(-75.19273854113067,39.95235140309713),4326),3857)))
  FROM indego_station_statuses)
 
--- answer: 34th&Spruce 
+
