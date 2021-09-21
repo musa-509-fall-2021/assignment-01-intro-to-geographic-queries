5a70a190291e0505792b1c44aaf8813fe61ebb0f
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-20 21:56:45 -0400

    Update query09.sql

--- query09.sql
+++ query09.sql
@@ -6,7 +6,8 @@
 */
 
 -- Enter your SQL query here
-SELECT type, count
+WITH new_table AS(
+ SELECT type, count
 FROM(
   SELECT passholder_type AS type, COUNT(passholder_type) AS count
 FROM indego_trips_2019_q2
@@ -15,10 +16,21 @@ GROUP BY passholder_type
   SELECT passholder_type AS type, COUNT(passholder_type) AS count
 FROM indego_trips_2020_q2
 GROUP BY passholder_type
-  ) AS Q9
+  ) AS Q9)
+SELECT type AS type_1, SUM(count) as count_1 
+FROM new_table
+GROUP BY type
 
 /*
 Answer:
+In total, it is:
+Day Pass: 72362
+Indego30: 263249
+Indego365: 56358
+IndegoFlex: 852
+NULL: 35
+Walk-up: 84
+
 For year 2019, it is:
 Day Pass: 34197
 Indego30: 133344
