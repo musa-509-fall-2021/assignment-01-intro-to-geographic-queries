35decd5a6a980c02206ae142c2eafe9eb2a12bab
ShiminTu <89991242+ShiminTu@users.noreply.github.com>
2021-09-20 21:30:51 -0400

    Update query07.sql
    2019:1600
    2020:2557

--- query07.sql
+++ query07.sql
@@ -3,8 +3,16 @@
 */
 
 -- Enter your SQL query here
-SELECT count(*) 
-FROM indego_trips_2020_q2
+with union_table as (
+  SELECT a.trip_id,a.duration,a.start_time::date,a.end_time::date,2019 as year
+  FROM indego_trips_2019_q2 a
+  UNION
+  SELECT b.trip_id,b.duration,b.start_time::date,b.end_time::date,2020 as year
+  FROM indego_trips_2020_q2 b)
+ 
+SELECT count(*),year
+FROM union_table
 WHERE EXTRACT(DAY FROM end_time::date)-EXTRACT(DAY FROM start_time::date) = 1
+GROUP BY 2
 
---result:2557
+
