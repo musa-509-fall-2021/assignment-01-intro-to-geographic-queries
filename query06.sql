dcb1ef393380fb3818ec0157ac1590a3922e8dd1
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-20 20:06:53 -0400

    Update query06.sql

--- query06.sql
+++ query06.sql
@@ -1,8 +1,12 @@
 /*
   How many trips were shorter than 10 minutes?
-  ANSWER: 74958 for year 2019 and 46729 for year 2020.
+  ANSWER: 46729 for year 2019 and 74958 for year 2020.
 */
 
 -- Enter your SQL query here
-SELECT COUNT(duration) FROM indego_trips_2019_q2 WHERE duration < 10
-SELECT COUNT(duration) FROM indego_trips_2020_q2 WHERE duration < 10
+SELECT trip_less_than_10min
+FROM(
+  SELECT COUNT(duration) AS trip_less_than_10min FROM indego_trips_2019_q2 WHERE duration < 10
+  UNION
+  SELECT COUNT(duration) AS trip_less_than_10min FROM indego_trips_2020_q2 WHERE duration < 10
+  ) AS Q6
